ec33f0f7e0c0c3e7545117ade979e652fd6e304a
MuramatsuHaruto <muraharu2304@gmail.com>
2023-09-12 10:07:53 +0900

    AppUserController.php

--- Etc/Laravel/web.php
+++ Etc/Laravel/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/app_user/{id}', [AppUserController::class, 'app_user'])->name('al.app_user');
+
 Route::get('/', function () {
     return view('welcome');
 });
